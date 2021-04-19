4270157ff45e4f1ffd22f14c1878acae4d5c2af7
maricorla <marinagulevichqa@gmail.com>
2021-04-19 19:28:10 +0200

    forms style

---
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserSettings } from '../data/user-settings';
+
+@Component({
+  selector: 'app-user-settings-form',
+  templateUrl: './user-settings-form.component.html',
+  styleUrls: ['./user-settings-form.component.css']
+})
+export class UserSettingsFormComponent implements OnInit {
+
+  originalUserSettings: UserSettings = {
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes:null
+  }
+  userSettings: UserSettings = { ...this.originalUserSettings };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onsubmit: ', form.valid)
+  }
+
+}
